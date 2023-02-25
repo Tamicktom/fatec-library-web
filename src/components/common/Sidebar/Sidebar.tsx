1efff9632620ec03e65ba77da6bbb509671cbf9f
Tamicktom <rickpokemon38@gmail.com>
2023-02-25 13:34:11 -0300

    save

--- src/components/common/Sidebar/Sidebar.tsx
+++ src/components/common/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 "use client";
 //* Libraries imports
+import { useState, useEffect } from "react";
 
 type Props = {
 
@@ -8,33 +9,56 @@ type Props = {
 export default function Sidebar(props: Props) {
   return (
     <div className='flex flex-col items-center justify-start h-full py-4 w-96'>
-      <div className="flex flex-col items-center justify-start w-full gap-4 p-4 bg-gray-200/50">
-        <span className="text-xl font-bold text-gray-900">
-          Faça login em sua conta:
-        </span>
-        <div className="flex flex-col items-center justify-start w-full gap-4">
-          <div className="flex flex-col items-center justify-start w-full gap-1">
-            <label className="text-lg font-bold text-gray-900" htmlFor="email">Email</label>
-            <input
-              id="email"
-              type="text"
-              className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-            />
+      <Login />
+    </div>
+  );
+}
+
+function Login() {
+  const [loginCredentials, setLoginCredentials] = useState({ email: '', password: '' });
+  const [isLogged, setIsLogged] = useState(false);
+
+  return (
+    <div className="flex flex-col items-center justify-start w-full gap-4 p-4 bg-gray-200/50">
+      {
+        isLogged
+          ?
+          <div className="flex items-center justify-start gap-4 flexx-row">
+            <img className="w-16 h-16 rounded-full" src="https://avatars.githubusercontent.com/u/4256471?v=4" alt="Avatar" />
+            <span className="text-xl font-bold text-gray-900"> Olá, Usuário </span>
           </div>
-          <div className="flex flex-col items-center justify-start w-full gap-1">
-            <label className="text-lg font-bold text-gray-900" htmlFor="password">Senha</label>
-            <input
-              id="password"
-              className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-              type="password"
-              security="true"
-            />
-          </div>
-        </div>
-        <button className="w-full p-2 mt-4 text-sm font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
-          Entrar
-        </button>
-      </div>
+          : <>
+            <span className="text-xl font-bold text-gray-900">
+              Faça login em sua conta:
+            </span>
+            <div className="flex flex-col items-center justify-start w-full gap-4">
+              <div className="flex flex-col items-center justify-start w-full gap-1">
+                <label className="text-lg font-bold text-gray-900" htmlFor="email">Email</label>
+                <input
+                  onChange={(e) => setLoginCredentials({ ...loginCredentials, email: e.target.value })}
+                  id="email"
+                  type="text"
+                  className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-start w-full gap-1">
+                <label className="text-lg font-bold text-gray-900" htmlFor="password">Senha</label>
+                <input
+                  onChange={(e) => setLoginCredentials({ ...loginCredentials, password: e.target.value })}
+                  id="password"
+                  className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                  type="password"
+                  security="true"
+                />
+              </div>
+            </div>
+            <button
+              onClick={() => setIsLogged(true)}
+              className="w-full p-2 mt-4 text-sm font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+              Entrar
+            </button>
+          </>
+      }
     </div>
   );
 }
