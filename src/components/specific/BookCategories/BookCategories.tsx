4ec0afd8726ea97eec3a402b47f8a52b44cfd61e
Tamicktom <rickpokemon38@gmail.com>
2023-02-25 14:12:35 -0300

    center books

--- src/components/specific/BookCategories/BookCategories.tsx
+++ src/components/specific/BookCategories/BookCategories.tsx
@@ -51,7 +51,7 @@ function Category(props: Categorie) {
   return (
     <div className="flex flex-col items-start justify-start w-full gap-2 transition-all">
       <h1 className="text-2xl font-bold text-gray-800">{props.title}</h1>
-      <div className="flex flex-row items-start justify-start w-full gap-8 transition-all">
+      <div className="flex flex-row items-start justify-between w-full gap-8 transition-all">
         {
           props.fiveBooks.length > 0
             ? props.fiveBooks.map((book, index) => {
